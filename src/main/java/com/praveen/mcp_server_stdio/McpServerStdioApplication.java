a171e1c07cda5b32a9563c314f73bdc6ad4b6511
praveenkumar09 <tpkprav@gmail.com>
2025-11-17 17:14:59 +0800

    Initial commit

---
+++ src/main/java/com/praveen/mcp_server_stdio/McpServerStdioApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.mcp_server_stdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServerStdioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpServerStdioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/mcp_server_stdio/McpServerStdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.mcp_server_stdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerStdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
