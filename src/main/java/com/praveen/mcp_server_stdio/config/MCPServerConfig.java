c0c5b34b3bbd668dfc8ca7f7e5b26f33d2f2d6c9
praveenkumar09 <tpkprav@gmail.com>
2025-11-17 17:37:44 +0800

    MCP Server Config added

---
+++ src/main/java/com/praveen/mcp_server_stdio/config/MCPServerConfig.java
@@ -0,0 +1,18 @@
+package com.praveen.mcp_server_stdio.config;
+
+import com.praveen.mcp_server_stdio.tool.HelpDeskTools;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class MCPServerConfig {
+
+    @Bean
+    public List<ToolCallback> toolCallbacks(HelpDeskTools helpDeskTools){
+        return List.of(ToolCallbacks.from(helpDeskTools));
+    }
+}
